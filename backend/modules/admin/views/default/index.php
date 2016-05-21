60a1ae1a7f58fe63b77b209d03c98d0961d10328
Mr.Wang <wangrunxinyes@gmail.com>
2016-05-21 20:43:38 +0800

    -commit;

--- backend/modules/admin/views/default/index.php
+++ backend/modules/admin/views/default/index.php
@@ -3,9 +3,9 @@ use yii\helpers\Url;
 
 ?>
 <style>
-  .btn-lg{
-  margin-right: 10px;
-  }
+.btn-lg {
+	margin-right: 10px;
+}
 </style>
 <div class="admin-default-index">
 	<p>Functional Pannel</p>
@@ -13,18 +13,18 @@ use yii\helpers\Url;
 		href="<?php echo Url::to(['default/message']); ?>">Message Control</a>
 	<a class="btn btn-lg btn-success"
 		href="<?php echo Url::to(['default/visitor']); ?>">Visitor Control</a>
-	<BR>
-	<BR>
+	<a class="btn btn-lg btn-success"
+		href="<?php echo Url::to(['default/vip']); ?>">VIP Control</a>
+	<BR> <BR>
 	<p>Blog Pannel</p>
 	<a class="btn btn-lg btn-success"
 		href="<?php echo Url::to(['../blog/blog-catalog']); ?>">Edit Category</a>
 	<a class="btn btn-lg btn-success"
-		href="<?php echo Url::to(['../blog/blog-post']); ?>">Edit Post</a>
-	<a class="btn btn-lg btn-success"
+		href="<?php echo Url::to(['../blog/blog-post']); ?>">Edit Post</a> <a
+		class="btn btn-lg btn-success"
 		href="<?php echo Url::to(['../blog/blog-comment']); ?>">Edit Comment</a>
 	<a class="btn btn-lg btn-success"
-		href="<?php echo Url::to(['../blog/blog-tag']); ?>">Edit TAG</a>
-	<BR>
+		href="<?php echo Url::to(['../blog/blog-tag']); ?>">Edit TAG</a> <BR>
 	<BR>
 	<p>Amdin Control Pannel</p>
 	<a class="btn btn-lg btn-success"
@@ -35,7 +35,6 @@ use yii\helpers\Url;
 		href="<?php echo Url::to(['default/flog']); ?>">Show Frontend Log</a>
 	<a class="btn btn-lg btn-success"
 		href="<?php echo Url::to(['default/refreshasset']); ?>">Refresh assets</a>
-	<BR>
-	<BR> <BR>
+	<BR> <BR> <BR>
 
 </div>
